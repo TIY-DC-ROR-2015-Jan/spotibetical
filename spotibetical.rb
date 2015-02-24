5849d9353932da0db5a16b0d3acc40c1286ba1f8
Wonman Lee <wonmanlee@gmail.com>
2015-02-24 17:34:05 -0500

    Modified the edit profile page and endpoint

--- spotibetical.rb
+++ spotibetical.rb
@@ -32,28 +32,34 @@ class Spotibetical < Sinatra::Base
     erb :user_profile_edit
   end
 
-   patch '/users/profile/edit' do
+  patch '/users/profile/edit' do
     if current_user
       u = current_user
       if params["avatar_url"]
         u.avatar_url=params["avatar_url"]
+        u.save!
       end 
-    #   if params["bio"]
-    #     u.bio=params["bio"]
-    #   end
-    #   if params["home_state"]
-    #     u.home_state=params["home_state"]
-    #   end
-    #   if params["zodiac_sign"]
-    #     u.zodiac_sign=params["zodiac_sign"]
-    #   end
-    #   redirect to ('/users/profile')
-    # else
-    #   redirect to('/users/login')
+      if params["bio"]
+        u.bio=params["bio"]
+        u.save!
+      end
+      if params["home_state"]
+        u.home_state=params["home_state"]
+        u.save!
+      end
+      if params["zodiac_sign"]
+        u.zodiac_sign=params["zodiac_sign"]
+        u.save!
+      end
+      if params["favorite_song_url"]
+        u.favorite_song_url=params["favorite_song_url"]
+        u.save!
+      end
+    else
+      redirect to('/users/login')
     end
     redirect to('/users/profile')
   end
-
 
   post '/users/login' do
     user = User.where(
