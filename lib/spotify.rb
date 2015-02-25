caa87d4c7bb317615b58216aee6cc77019583669
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-25 15:33:24 -0500

    Built methods for creating playlist

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,7 +1,25 @@
-class  Spot
 require 'httparty'
-
+require 'pry'
+class  Spot
+  @authorization =''
   def self.find_song spotify_uri
     HTTParty.get("https://api.spotify.com/v1/tracks/#{spotify_uri}")
   end
+
+
+  def self.create_spotify_playlist 
+    HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists", 
+      headers: {'Authorization' => "Bearer #{@authorization}",
+                'Content-Type' => 'application/json'},
+      body:    {'name' =>'test', 
+                'public' => 'true'})
+  end
+
+  def self.add_tracks_to_spotify playlist_id, spotify_uris
+    HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists/#{playlist_id}/tracks", 
+                  header:{'Authorization' => "Bearer #{@authorization}",
+                  'Content-Type' => 'application/json'},
+                  body: {"uris" => spotify_uris})
+  end
+
 end
