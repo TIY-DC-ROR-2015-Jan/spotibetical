d4475e33a6095f9ba0ce44d60d89c499e9a745da
James Dabbs <jamesdabbs@gmail.com>
2015-02-25 13:29:42 -0500

    Fix various validation failures

--- test/user_test.rb
+++ test/user_test.rb
@@ -19,7 +19,7 @@ class UserTest < MiniTest::Test
   def test_users_can_have_songs
 
     u = User.create! name: 'Brit Butler', email: 'brit@kingcons.io', password: 'password'
-    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1
+    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1, spotify_id: '-'
     u.songs.push s
     # playlist.songs.push s
 
@@ -29,14 +29,14 @@ class UserTest < MiniTest::Test
 
   def test_songs_save_vote_counts
     u = User.create! name: 'Brit Butler', email: 'brit@kingcons.io', password: 'password'
-    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1
+    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1, spotify_id: '-'
     Vote.create!(song_id: s.id, user_id: u.id)
     assert_equal s.votes.count, 1
   end
 
   def test_vote_count_decreases_after_vote
     u = User.create! name: 'Brit Butler', email: 'brit@kingcons.io', password: 'password'
-    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1
+    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1, spotify_id: '-'
     u.votes.create!(song_id: s.id)
     u.vote_count -= 1
     u.save!
@@ -45,7 +45,7 @@ class UserTest < MiniTest::Test
 
   def test_user_cannot_vote_more_than_vote_count
     u = User.create! name: 'Brit Butler', email: 'brit@kingcons.io', password: 'password'
-    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1
+    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1, spotify_id: '-'
     u.vote_count = 1
     u.vote [s.id, s.id]
     assert_equal s.votes.count, 0
@@ -53,7 +53,7 @@ class UserTest < MiniTest::Test
 
   def test_vote_handler_works
     u = User.create! name: 'Brit Butler', email: 'brit@kingcons.io', password: 'password'
-    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1
+    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1, spotify_id: '-'
     post '/users/login', email: 'brit@kingcons.io', password: 'password'
     patch '/vote', songs: [s.id]
     assert_equal s.votes.count, 1
@@ -61,7 +61,7 @@ class UserTest < MiniTest::Test
 
   def test_veto_handler_works
     u = User.create! name: 'Brit Butler', email: 'brit@kingcons.io', password: 'password'
-    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1
+    s = Song.create! artist: 'The Faint', title: 'Dress Code', user_id: 1, spotify_id: '-'
     post '/users/login', email: 'brit@kingcons.io', password: 'password'
     patch '/veto', song_id: s.id
     assert_equal Song.find(s.id).veto, true
--- test/view_suggestions_test.rb
+++ test/view_suggestions_test.rb
@@ -8,24 +8,24 @@ class ViewSuggestionsTest < MiniTest::Test
   end
 
   def test_songs_have_artists
-    x = Song.create! artist: 'abc', title: '123', spotify_link: 'google.com', user_id: 1
+    x = Song.create! artist: 'abc', title: '123', spotify_link: 'google.com', user_id: 1, spotify_id: '-'
 
     assert_match x.artist, "abc"
   end
 
   def test_songs_have_ids
-    x = Song.create! artist: 'jbc', title: '123', spotify_link: 'google.com', user_id: 2
+    x = Song.create! artist: 'jbc', title: '123', spotify_link: 'google.com', user_id: 2, spotify_id: '-'
 
     assert x.id
     assert x.class == Integer
   end
 
   def test_songs_can_be_sorted
-    Song.create! artist: 'jbc', title: '123', spotify_link: 'google.com', user_id: 2
-    Song.create! artist: 'acb', title: '123', spotify_link: 'google.com', user_id: 3
-    Song.create! artist: 'bbc', title: '123', spotify_link: 'google.com', user_id: 4
-    Song.create! artist: 'cbc', title: '123', spotify_link: 'google.com', user_id: 5
-    Song.create! artist: 'abd', title: '123', spotify_link: 'google.com', user_id: 6
+    Song.create! artist: 'jbc', title: '123', spotify_link: 'google.com', spotify_id: '-', user_id: 2
+    Song.create! artist: 'acb', title: '123', spotify_link: 'google.com', spotify_id: '--', user_id: 3
+    Song.create! artist: 'bbc', title: '123', spotify_link: 'google.com', spotify_id: '---', user_id: 4
+    Song.create! artist: 'cbc', title: '123', spotify_link: 'google.com', spotify_id: '----', user_id: 5
+    Song.create! artist: 'abd', title: '123', spotify_link: 'google.com', spotify_id: '------', user_id: 6
 
     #Not sure what to write here since display thing 
   end
