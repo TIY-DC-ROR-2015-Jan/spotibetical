e9ca70a7f01d92038ca98befd11277ac1b1eddf1
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-25 16:16:47 -0500

    Removed pry

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,25 +1,29 @@
 require 'httparty'
-require 'pry'
 class  Spot
-  @authorization =''
+
+
+  spotify_client_id = "id1"
+  spotify_secret_id = "id2"
+  spotify_authorization = "id3"
+
   def self.find_song spotify_uri
     HTTParty.get("https://api.spotify.com/v1/tracks/#{spotify_uri}")
   end
 
 
-  def self.create_spotify_playlist 
+  def self.create_spotify_playlist authorization
     HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists", 
-      headers: {'Authorization' => "Bearer #{@authorization}",
+      headers: {'Authorization' => "Bearer #{authorization}",
                 'Content-Type' => 'application/json'},
       body:    {'name' =>'test', 
                 'public' => 'true'})
   end
 
-  def self.add_tracks_to_spotify playlist_id, spotify_uris
+  def self.add_tracks_to_spotify playlist_id, spotify_uris, authorization
+    binding.pry
     HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists/#{playlist_id}/tracks", 
-                  header:{'Authorization' => "Bearer #{@authorization}",
+                  header:{'Authorization' => "Bearer #{authorization}",
                   'Content-Type' => 'application/json'},
                   body: {"uris" => spotify_uris})
   end
-
 end
