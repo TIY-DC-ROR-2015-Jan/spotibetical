fb5d75646a15f543ce3e02c7148044f9a1eaf5d0
James Dabbs <jamesdabbs@gmail.com>
2015-02-25 19:56:21 -0500

    Add Spot#access_token method
    After a one-time setup (via the included `get_spotify_token.rb` script,
    this method _should_ auto-refresh and return a valid access token.

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -4,4 +4,23 @@ require 'httparty'
   def self.find_song spotify_uri
     HTTParty.get("https://api.spotify.com/v1/tracks/#{spotify_uri}")
   end
+
+  def self.refresh_access_token refresh_token
+    response = HTTParty.post("https://accounts.spotify.com/api/token", {
+      body: {
+        grant_type:    "refresh_token",
+        refresh_token: refresh_token,
+        client_id:     ENV.fetch("SPOTIFY_ID"),
+        client_secret: ENV.fetch("SPOTIFY_SECRET")
+      }
+    })
+    raise "Refresh error: #{response}" unless response.ok?
+    response
+  end
+
+  def self.access_token
+    t = SpotifyAccessToken.last
+    t.refresh! if t.expired?
+    t.access_token
+  end
 end
