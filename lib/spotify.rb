5c664b5a90d0959039cf2449cc68a01310b43af4
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-26 08:55:56 -0500

    Fixed syntax error

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -7,18 +7,19 @@ class  Spot
 
   def self.create_spotify_playlist name
     HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists", 
-                  headers: {'Authorization' => "Bearer #{Spot.access_token}",
-                            'Content-Type'  => 'application/json'},
-                  body:    {'name'   => name, 
-                            'public' => 'true'})
+      headers: {'Authorization' => "Bearer #{Spot.access_token}",
+      'Content-Type'  => 'application/json'},
+      body:    {'name'   => name, 
+        'public' => 'true'})
   end
 
   def self.add_tracks_to_spotify playlist_name, spotify_uris
     HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists/#{playlist_name}/tracks", 
-                  header:{'Authorization' => "Bearer #{Spot.access_token}",
-                          'Content-Type'  => 'application/json'},
-                  body:  {"uris" => spotify_uris})
-
+      header:{'Authorization' => "Bearer #{Spot.access_token}",
+      'Content-Type'  => 'application/json'},
+      body:  {"uris" => spotify_uris})
+  end
+  
   def self.refresh_access_token refresh_token
     response = HTTParty.post("https://accounts.spotify.com/api/token", {
       body: {
@@ -27,7 +28,7 @@ class  Spot
         client_id:     ENV.fetch("SPOTIFY_ID"),
         client_secret: ENV.fetch("SPOTIFY_SECRET")
       }
-    })
+      })
     raise "Refresh error: #{response}" unless response.ok?
     response
   end
