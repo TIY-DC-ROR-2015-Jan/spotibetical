3e7cdb324534620e9d78c86aaf5120cd765bddb0
James Dabbs <jamesdabbs@gmail.com>
2015-02-26 15:32:24 -0500

    Add User model for data migration
    Since the migration doesn't have access to the Rails env

--- db/migrate/20150226150154_encrypt_passwords.rb
+++ db/migrate/20150226150154_encrypt_passwords.rb
@@ -1,4 +1,7 @@
 class EncryptPasswords < ActiveRecord::Migration
+  class User < ActiveRecord::Base
+  end
+
   def change
     User.find_each do |u|
       u.password = Digest::SHA1.hexdigest(u.password)
