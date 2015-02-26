15d1bec48b15ce2c92634e03ceca03622857a5ff
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-26 13:57:46 -0500

    Refactored

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,15 +1,15 @@
 require 'httparty'
 
 class Spotify
-
-  URI = 'https://api.spotify.com/v1'
-  # @headers = {
-  #   'Authorization' => "Bearer #{Spotify.access_token}",
-  #   'Content-Type'  => 'application/json'
-  # }
+  def self.header access_token
+    header = {
+      'Authorization' => "Bearer #{access_token}",
+      'Content-Type'  => 'application/json'
+      }
+  end
 
   def self.find_song spotify_uri
-    song = HTTParty.get("#{URI}/tracks/#{spotify_uri}")
+    song = HTTParty.get("https://api.spotify.com/v1/tracks/#{spotify_uri}")
     song_hash = {
       artist: song["artists"][0]["name"],
       track_name: song["name"],
@@ -20,16 +20,9 @@ class Spotify
 
 
   def self.create_spotify_playlist playlist
-    response = HTTParty.post("#{URI}/users/dcironyard/playlists", 
-      headers: {
-        'Authorization' => "Bearer #{Spotify.access_token}",
-        'Content-Type'  => 'application/json'
-        },
-        body:{
-          'name' => playlist.name, 
-          'public' => 'true'
-          }.to_json
-          )
+    response = HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists", 
+      headers: Spotify.header(Spotify.access_token),
+      body:{'name' => playlist.name}.to_json)
     playlist.update(
       spotify_link: response['external_urls']['spotify'],
       spotify_id: response['id']
@@ -37,14 +30,10 @@ class Spotify
   end
 
   def self.add_tracks_to_spotify playlist_spotify_id, spotify_uris
-    HTTParty.post("#{URI}/users/dcironyard/playlists/#{playlist_spotify_id}/tracks", 
-      headers: {
-        'Authorization' => "Bearer #{Spotify.access_token}",
-        'Content-Type'  => 'application/json'
-        },
-        body:  {"uris" => spotify_uris
-          }.to_json
-          )
+    HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists/#{playlist_spotify_id}/tracks", 
+      headers: Spotify.header(Spotify.access_token),
+      body:  {"uris" => spotify_uris}.to_json
+      )
   end
 
   def self.refresh_access_token refresh_token
