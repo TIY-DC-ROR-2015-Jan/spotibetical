9c6dd972111245f68dc5d8d51ee79f106b72c6ad
Katie Walsh <katie.katiewalsh@gmail.com>
2015-02-26 14:00:09 -0500

    New erb for song_added

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -3,12 +3,14 @@ require 'httparty'
 class Spotify
   def self.find_song spotify_uri
     song = HTTParty.get("https://api.spotify.com/v1/tracks/#{spotify_uri}")
+    binding.pry
     song_hash = {
       artist: song["artists"][0]["name"],
       track_name: song["name"],
       play_link: song["external_urls"]["spotify"],
       preview_link: song["preview_url"]
     }
+    binding.pry
   end
 
   def self.create_spotify_playlist name
