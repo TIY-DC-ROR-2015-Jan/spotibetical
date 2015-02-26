42a9dabe701ddf0bc6c7844dc3be5161a80896a8
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-25 23:19:58 -0500

    Updated for new auth methods

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,29 +1,22 @@
 require 'httparty'
 class  Spot
 
-
-  spotify_client_id = "id1"
-  spotify_secret_id = "id2"
-  spotify_authorization = "id3"
-
   def self.find_song spotify_uri
     HTTParty.get("https://api.spotify.com/v1/tracks/#{spotify_uri}")
   end
 
-
-  def self.create_spotify_playlist authorization
+  def self.create_spotify_playlist name
     HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists", 
-      headers: {'Authorization' => "Bearer #{authorization}",
-                'Content-Type' => 'application/json'},
-      body:    {'name' =>'test', 
-                'public' => 'true'})
+                  headers: {'Authorization' => "Bearer #{Spot.access_token}",
+                            'Content-Type'  => 'application/json'},
+                  body:    {'name'   => name, 
+                            'public' => 'true'})
   end
 
-  def self.add_tracks_to_spotify playlist_id, spotify_uris, authorization
-    binding.pry
-    HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists/#{playlist_id}/tracks", 
-                  header:{'Authorization' => "Bearer #{authorization}",
-                  'Content-Type' => 'application/json'},
-                  body: {"uris" => spotify_uris})
+  def self.add_tracks_to_spotify playlist_name, spotify_uris
+    HTTParty.post("https://api.spotify.com/v1/users/dcironyard/playlists/#{playlist_name}/tracks", 
+                  header:{'Authorization' => "Bearer #{Spot.access_token}",
+                          'Content-Type'  => 'application/json'},
+                  body:  {"uris" => spotify_uris})
   end
 end
